6c97fb096399e347a27384133cdcf2cd2364443a
dan <dan@ELSYIUM-PC>
2026-07-10 11:26:46 +0100

    Make preview position binding trim safe

--- Infinity.Shell.WinUI/Views/PreviewPositionView.xaml.cs
+++ Infinity.Shell.WinUI/Views/PreviewPositionView.xaml.cs
@@ -8,4 +8,21 @@ public sealed partial class PreviewPositionView :
     public PreviewPositionView() => InitializeComponent();
 
     public PreviewPositionViewModel ViewModel => (PreviewPositionViewModel)DataContext;
+
+    public int SelectedPositionIndex
+    {
+        get => ViewModel.Value switch
+        {
+            (int)PreviewPosition.Auto => 0,
+            (int)PreviewPosition.Top => 1,
+            (int)PreviewPosition.Bottom => 2,
+            _ => 0
+        };
+        set => ViewModel.Value = value switch
+        {
+            1 => (int)PreviewPosition.Top,
+            2 => (int)PreviewPosition.Bottom,
+            _ => (int)PreviewPosition.Auto
+        };
+    }
 }
